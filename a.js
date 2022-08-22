2e6d854ecbe4c760815cd73a6ff6086db5bcdadb
Tony <36411557+Saccharide@users.noreply.github.com>
2022-08-22 15:06:20 -0400

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,18 @@
+
+function sendXHR(type, url, data, callback) {
+  var newXHR = new XMLHttpRequest() || new window.ActiveXObject("Microsoft.XMLHTTP");
+  newXHR.open(type, url, true);
+  newXHR.send(data);
+  newXHR.onreadystatechange = function() {
+    if (this.status === 200 && this.readyState === 4) {
+      callback(this.response);
+    }
+  };
+}
+
 function a(){
-  document.write('<iframe src=file://admin.j2></iframe>');
-  alert(2);
+sendXHR("GET", "admin.html", null, function(response) { // response contains the content of the description.txt file.
+ console.log(response); // Use innerHTML to get or set the html content.
+  alert(3);
+});
 }
